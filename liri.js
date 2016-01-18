4d23b9c46055f934ce89122e5b3c843f1e107ad6
steph-harris <steph.harris21@gmail.com>
2016-01-18 02:58:23 -0500

    getting a response from twitter api

--- liri.js
+++ liri.js
@@ -1,12 +1,23 @@
 debugger;
+var Twitter = require('twitter');
 var keys = require("./keys.js");
 var task = process.argv[2];
 var twitterConsKey = twitterKeys.consumer_key;
 var twitterSecret = twitterKeys.consumer_secret;
 var twitterTokKey = twitterKeys.access_token_key;
 var twitterTokSecret = twitterKeys.access_token_secret;
-console.log(twitterSecret);
-console.log(twitterConsKey);
-console.log(twitterTokKey);
-console.log(twitterTokSecret);
+
+var client = new Twitter({
+  consumer_key: twitterConsKey,
+  consumer_secret: twitterSecret,
+  access_token_key: twitterTokKey,
+  access_token_secret: twitterTokSecret,
+});
+
+var params = {screen_name: "steph_harris21"};
+client.get('statuses/user_timeline', function(error, tweets, response){
+  if(error) throw error;
+  console.log(tweets);  // The tweets. 
+  //console.log(response);  // Raw response object. 
+});
 console.log(task);
